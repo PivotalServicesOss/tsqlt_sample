fca84b91a8004a6ef4722a7640b6cfb807e18333
Alfus Inigo Jaganathan <ajaganathan@vmware.com>
2022-03-27 00:43:49 -0400

    converted to use fixture

--- database/Sample.DB.TsqlTests/TsqlUnitTests.cs
+++ database/Sample.DB.TsqlTests/TsqlUnitTests.cs
@@ -1,124 +1,16 @@
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
-[TestCaseOrderer("AlphabeticalOrderer", "Sample.DB.TsqlTests")]
-public class TsqlUnitTests
+public class TsqlUnitTests : IClassFixture<TsqltTestFactory>
 {
-    private const string TSQLT_FOLDER_NM = "tSQLt_V1.0.8083.3529";
-    private const string TSQLT_PREPARE_SERVER_SQL_FILE = "PrepareServer.sql";
-    private const string TSQLT_FRAMEWORK_SQL_FILE = "tSQLt.class.sql";
-    private const string DB_CONNECTION_STR_ENV_VAR_NM = "ConnectionStrings:SampleDatabase";
-    private readonly string rootDirectory = Directory.GetParent(typeof(TsqlUnitTests).Assembly.Location).FullName;
-    private readonly ITestOutputHelper testOutputLogger;
+    private readonly TsqltTestFactory factory;
 
-    public TsqlUnitTests(ITestOutputHelper testOutputLogger)
+    public TsqlUnitTests(TsqltTestFactory factory, ITestOutputHelper testOutputLogger)
     {
-        this.testOutputLogger = testOutputLogger;
+        this.factory = factory;
+        factory.Initialize(testOutputLogger);
     }
 
     [Fact]
-    public async Task A_PrepareSqlServer()
+    public async Task ExecuteAllTsqltTests()
     {
-        var prepareServerSqlFile = Path.Combine(rootDirectory, TSQLT_FOLDER_NM, TSQLT_PREPARE_SERVER_SQL_FILE);
-        var prepareServerSql = File.ReadAllLines(prepareServerSqlFile);
-
-        WriteOutput($"Executing sql file {prepareServerSqlFile}");
-
-        using var sqlConnection = GetSqlConnection();
-        await ExecuteScriptsInChunks(sqlConnection, prepareServerSql);
-    }
-
-    [Fact]
-    public async Task B_ConfigureTsqltFramework()
-    {
-        var tSQLtClassSqlFile = Path.Combine(rootDirectory, TSQLT_FOLDER_NM, TSQLT_FRAMEWORK_SQL_FILE);
-        var tSQLtClassSql = File.ReadAllLines(tSQLtClassSqlFile);
-
-        WriteOutput($"Executing sql file {tSQLtClassSqlFile}");
-
-        using var sqlConnection = GetSqlConnection();
-        await ExecuteScriptsInChunks(sqlConnection, tSQLtClassSql);
-    }
-
-    [Fact]
-    public async Task C_DeployAllTsqltTests()
-    {
-        var tsqltTestFiles = Directory.GetFiles(rootDirectory, "*Tests.sql", SearchOption.AllDirectories);
-
-        using var sqlConnection = GetSqlConnection();
-        foreach (var tsqltTestFile in tsqltTestFiles)
-        {
-            WriteOutput($"Executing sql file {tsqltTestFile}");
-            var tsqltTestFileSql = File.ReadAllLines(tsqltTestFile);
-            await ExecuteScriptsInChunks(sqlConnection, tsqltTestFileSql);
-        }
-    }
-
-    [Fact]
-    public async Task D_ExecuteAllTsqltTests()
-    {
-        using var sqlConnection = GetSqlConnection();
-        await ExecuteNonQueryAsync(sqlConnection, "EXEC tSQLt.RunAll;");
-    }
-
-    private async Task ExecuteScriptsInChunks(SqlConnection sqlConnection, string[] sqlContent)
-    {
-        var sqlBuilder = new StringBuilder();
-        foreach (var line in sqlContent)
-        {
-            if (line.Trim().ToLower() == "go")
-            {
-                await ExecuteNonQueryAsync(sqlConnection, sqlBuilder.ToString());
-                sqlBuilder = new StringBuilder();
-            }
-            else
-                sqlBuilder.AppendLine(line);
-        }
-    }
-
-    private async Task<int> ExecuteNonQueryAsync(SqlConnection sqlConnection, string sql)
-    {
-        using var cmd = new SqlCommand(sql, sqlConnection);
-        return await cmd.ExecuteNonQueryAsync();
-    }
-
-    private SqlConnection GetSqlConnection()
-    {
-        var sqlConnection = new SqlConnection(GetConnectionStringFromEnvironmentVariable());
-        sqlConnection.InfoMessage += (s, a) => WriteOutput(a);
-        sqlConnection.Open();
-        return sqlConnection;
-    }
-
-    private void WriteOutput(SqlInfoMessageEventArgs e)
-    {
-        Debug.WriteLine(e.Message);
-        Console.WriteLine(e.Message);
-        testOutputLogger.WriteLine(e.Message);
-    }
-
-    private void WriteOutput(string text)
-
-    {
-        Debug.WriteLine(text);
-        Console.WriteLine(text);
-        testOutputLogger.WriteLine(text);
-    }
-
-    private string GetConnectionStringFromEnvironmentVariable()
-    {
-        var connectionString = Environment.GetEnvironmentVariable(DB_CONNECTION_STR_ENV_VAR_NM);
-
-        if (string.IsNullOrWhiteSpace(connectionString))
-            return "Server=localhost,2000;Database=Sample;User Id=sa;Password=AlwaysBeKind@;";
-
-        return connectionString;
-    }
-}
-
-// Reference: https://docs.microsoft.com/en-us/dotnet/core/testing/order-unit-tests?pivots=xunit
-public class AlphabeticalOrderer : ITestCaseOrderer
-{
-    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
-    {
-        return testCases.OrderBy(testCase => testCase.TestMethod.Method.Name);
+        await factory.ExecuteNonQueryAsync("EXEC tSQLt.RunAll;");
     }
 }
---
+++ database/Sample.DB.TsqlTests/TsqltTestFactory.cs
@@ -0,0 +1,129 @@
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+public class TsqltTestFactory : IDisposable
+{
+    private readonly string rootDirectory = Directory.GetParent(typeof(TsqltTestFactory).Assembly.Location).FullName;
+    private bool _disposed;
+    private ITestOutputHelper testOutputLogger;
+    private SqlConnection connection;
+    private const string TSQLT_FOLDER_NM = "tSQLt_V1.0.8083.3529";
+    private const string TSQLT_PREPARE_SERVER_SQL_FILE = "PrepareServer.sql";
+    private const string TSQLT_FRAMEWORK_SQL_FILE = "tSQLt.class.sql";
+    private const string DB_CONNECTION_STR_ENV_VAR_NM = "ConnectionStrings:SampleDatabase";
+
+    public void Initialize(ITestOutputHelper testOutputLogger)
+    {
+        this.testOutputLogger = testOutputLogger;
+        connection = GetSqlConnection();
+        Task.WaitAll(PrepareSqlServer());
+        Task.WaitAll(ConfigureTsqltFramework());
+        Task.WaitAll(DeployAllTsqltTests());
+    }
+
+    private async Task PrepareSqlServer()
+    {
+        var prepareServerSqlFile = Path.Combine(rootDirectory, TSQLT_FOLDER_NM, TSQLT_PREPARE_SERVER_SQL_FILE);
+        var prepareServerSql = File.ReadAllLines(prepareServerSqlFile);
+
+        WriteOutput($"Executing sql file {prepareServerSqlFile}");
+        await ExecuteScriptsInChunks(prepareServerSql);
+    }
+
+    private async Task ConfigureTsqltFramework()
+    {
+        var tSQLtClassSqlFile = Path.Combine(rootDirectory, TSQLT_FOLDER_NM, TSQLT_FRAMEWORK_SQL_FILE);
+        var tSQLtClassSql = File.ReadAllLines(tSQLtClassSqlFile);
+
+        WriteOutput($"Executing sql file {tSQLtClassSqlFile}");
+        await ExecuteScriptsInChunks(tSQLtClassSql);
+    }
+
+    private async Task DeployAllTsqltTests()
+    {
+        var tsqltTestFiles = Directory.GetFiles(rootDirectory, "*Tests.sql", SearchOption.AllDirectories);
+
+        foreach (var tsqltTestFile in tsqltTestFiles)
+        {
+            WriteOutput($"Executing sql file {tsqltTestFile}");
+            var tsqltTestFileSql = File.ReadAllLines(tsqltTestFile);
+            await ExecuteScriptsInChunks(tsqltTestFileSql);
+        }
+    }
+
+    public async Task<int> ExecuteNonQueryAsync(string sql)
+    {
+        using var cmd = new SqlCommand(sql, connection);
+        return await cmd.ExecuteNonQueryAsync();
+    }
+
+    private async Task ExecuteScriptsInChunks(string[] sqlContent)
+    {
+        var sqlBuilder = new StringBuilder();
+        foreach (var line in sqlContent)
+        {
+            if (line.Trim().ToLower() == "go")
+            {
+                await ExecuteNonQueryAsync(sqlBuilder.ToString());
+                sqlBuilder = new StringBuilder();
+            }
+            else
+                sqlBuilder.AppendLine(line);
+        }
+    }
+
+    private SqlConnection GetSqlConnection()
+    {
+        var sqlConnection = new SqlConnection(GetConnectionStringFromEnvironmentVariable());
+        sqlConnection.InfoMessage += (s, a) => WriteOutput(a);
+        sqlConnection.Open();
+        return sqlConnection;
+    }
+
+    private void WriteOutput(SqlInfoMessageEventArgs e)
+    {
+        Debug.WriteLine(e.Message);
+        Console.WriteLine(e.Message);
+        testOutputLogger?.WriteLine(e.Message);
+    }
+
+    private void WriteOutput(string text)
+
+    {
+        Debug.WriteLine(text);
+        Console.WriteLine(text);
+        testOutputLogger?.WriteLine(text);
+    }
+
+    private string GetConnectionStringFromEnvironmentVariable()
+    {
+        var connectionString = Environment.GetEnvironmentVariable(DB_CONNECTION_STR_ENV_VAR_NM);
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+            return "Server=localhost,2000;Database=Sample;User Id=sa;Password=AlwaysBeKind@;";
+
+        return connectionString;
+    }
+
+    ~TsqltTestFactory()
+    {
+        Dispose(false);
+    }
+
+        public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing)
+        {
+            connection?.Close();
+        }
+
+        _disposed = true;
+    }
+}
